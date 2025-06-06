4bb6df71baad785fcf64a685ddb8bab170e5df5d
adanfime123 <adanfime1234@gmail.com>
2025-06-06 15:20:58 +0700

    commit

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 @echo off
 echo "Pushing to GitHub..."
 git add .
-git commit -m "update"
+git commit -m "commit"
 git push -u origin master
 echo "Push completed. Press any key to exit."
 pause > nul
